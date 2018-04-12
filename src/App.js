ff6571341ed4c6e9b63174f9143d91f1bfa2f58b
Hugo Zapata <hugozap@gmail.com>
2018-04-12 01:24:12 -0500

    v1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Knob } from "react-rotary-knob";
+import transitionSkin from './skins/transition1'
+import morphSkin from './skins/morph-path';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+          <Knob preciseMode={false} style={{width:'300px', height:'300px'}} min={0} max={100} skin={transitionSkin} />
+          <Knob preciseMode={false} style={{width:'300px', height:'300px'}} min={0} max={100} skin={morphSkin} />
+
+      </div>
+    );
+  }
+}
+
+export default App;
